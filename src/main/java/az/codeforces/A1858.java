01755a2b86841c1c563b873dd602c18a551fcb1c
FraserBogs <aliyevzakir814@gmail.com>
2023-10-19 22:41:50 +0400

    project

--- src/main/java/az/codeforces/A1858.java
+++ src/main/java/az/codeforces/A1858.java
@@ -8,6 +8,36 @@ public class A1858 {
 
         Scanner sc = new Scanner(System.in);
 
+        int t = sc.nextInt();
 
+        while (t-- != 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            boolean flag = c % 2 == 0;
+//            Bruteforce way
+//            while (a > 0 && b > 0) {
+//                if (flag) {
+//                    a--;
+//                    flag = false;
+//                } else {
+//                    b--;
+//                    flag = true;
+//                }
+//            }
+            if (flag) {
+                if (a>b) {
+                    System.out.println("First");
+                } else {
+                    System.out.println("Second");
+                }
+            } else {
+                if (a<b) {
+                    System.out.println("Second");
+                } else {
+                    System.out.println("First");
+                }
+            }
+        }
     }
 }
