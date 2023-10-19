b895cabf68ab7de86c73090bf12e7a5ae577c8c1
FraserBogs <aliyevzakir814@gmail.com>
2023-10-19 21:56:51 +0400

    project

---
+++ src/main/java/az/codeforces/A1858.java
@@ -0,0 +1,13 @@
+package az.codeforces;
+
+import java.util.Scanner;
+
+public class A1858 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
